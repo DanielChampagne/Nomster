80e859f0f4cf8cb373011f91a3a170385a47a281
Daniel Champagne <danchampagne@hotmail.com>
2019-09-23 01:51:01 +0000

    Preview Automated Email

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,7 +2,7 @@ class NotificationMailer < ApplicationMailer
 	default form: "no-reply@nomsterapp.com"
 
 	def comment_added
-		mail(to: "danchampagne@hotmail.com",
+		mail(to: "ajchampagne13@gmail.com",
 				subject: "A comment has been added to your place")
 	end
 end
